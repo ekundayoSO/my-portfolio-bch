d9922f6e3a9f6b8bae5027a7872d740b2673ec50
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-03-17 00:48:12 +0200

    my portfolio

---
+++ portfolio/main.js
@@ -0,0 +1,79 @@
+// // 
+
+// const backToTopBtn = document.querySelector(".backToTop");
+// const headerElement = document.querySelector("header");
+// const mobButton = document.querySelector(".mobile");
+// const nav = document.querySelector("nav");
+// const menuItems = document.querySelectorAll("nav ul li a");
+
+// function scrollFun() {
+//   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+//     backToTopBtn.style.display = "block";
+//     header.classList.add("bg");
+//   } else {
+//     backToTopBtn.style.display = "none";
+//     header.classList.remove("bg");
+    
+//   }
+// };
+
+// backToTopBtn.addEventListener("click", () => {
+//   document.body.scrollTop = 0; 
+//   document.documentElement.scrollTop = 0; 
+//   header.classList.toggle("bg");
+// });
+
+// window.addEventListener("scroll", scrollFun);
+// backToTopBtn.addEventListener("click", topFunction);
+
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+// const toggleMenu = () => {
+//   nav.classList.toggle("responsive");
+// };
+// mobButton.addEventListener("click", toggleMenu);
+// menuItems.forEach((item) => item.addEventListener("click", toggleMenu));
+
+
+const backToTopBtn = document.querySelector(".backToTop");
+const headerElement = document.querySelector("header");
+const mobButton = document.querySelector(".mobile");
+const nav = document.querySelector("nav ul");
+const menuItems = document.querySelectorAll("nav ul li a");
+
+function scrollFun() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    backToTopBtn.style.display = "block";
+    headerElement.classList.add("displayBtn");
+  } else {
+    backToTopBtn.style.display = "none";
+    headerElement.classList.remove("displayBtn");
+  }
+}
+
+backToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  headerElement.classList.toggle("displayBtn");
+});
+
+window.addEventListener("scroll", scrollFun);
+backToTopBtn.addEventListener("click", topFunction);
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const toggleMenu = () => {
+  nav.classList.toggle("responsive");
+};
+
+mobButton.addEventListener("click", toggleMenu);
+menuItems.forEach((item) => item.addEventListener("click", toggleMenu));
